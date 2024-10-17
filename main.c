749de63dadc7d26fe1cc3628b066bbec80cdc6fe
Louis Tsang <kawingt2@gmail.com>
2024-10-17 11:30:27 +0800

    started window with ncurses

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <ncurses.h>
 #include <time.h>
+
+void printing();
 
 int main (void)
 {
@@ -8,13 +11,22 @@ int main (void)
     int count = 60;
     double time_spent = 0;
 
-    // count down logic
-    do
-    {
-        printf("Counting down...  %f\n", time_spent);
-        time_spent = (double)(clock() - begin) / CLOCKS_PER_SEC;
-    }
-    while(time_spent <= 60);
+    initscr();
+
+    addstr("---------------------------- Hello ------------------------");
+    refresh();
+    addstr("\npress any key to exit...");
+
+    getch();
+    endwin();
+
+    // // count down logic
+    // do
+    // {
+    //     printf("Counting down...  %f\n", time_spent);
+    //     time_spent = (double)(clock() - begin) / CLOCKS_PER_SEC;
+    // }
+    // while(time_spent <= 60);
 
     // return when finished
     return 0;
