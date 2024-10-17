73ca0d3277f437a63c40af2f7b767f2e12383715
Louis Tsang <kawingt2@gmail.com>
2024-10-17 12:46:33 +0800

    wrote helper functions and accept command line arguements

--- main.c
+++ main.c
@@ -1,14 +1,27 @@
-#include <stdio.h>
 #include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-void printing();
+int convert_hour(int minute);
+int convert_minute(int minute);
+int convert_second(int minute);
 
-int main (void)
+int main (int argc, char* argv[])
 {
+    // check command line argument
+    if (argc < 2 || argc > 2)
+    {
+        printf("Usage: ./tcd MINUTES");
+        return 1;
+    }
+
     // initial set up
     clock_t begin = clock();
-    int count = 60;
+    int count = atoi(argv[1]);
+    int minute = convert_minute(count);
+    int hour = convert_hour(count);
+    int second = convert_second(count);
     double time_spent = 0;
     double prev_time = 0;
 
@@ -20,7 +33,7 @@ int main (void)
     // count down logic
     do
     {
-        mvprintw(10, 10, "00 : 00 : %02i", count);
+        mvprintw(10, 10, "%02i : %02i : %02i", hour, minute, second);
         refresh();
         time_spent = (double)(clock() - begin) / CLOCKS_PER_SEC;
         if (time_spent - prev_time >= 1)
@@ -37,6 +50,40 @@ int main (void)
     getch();
     endwin();
 
-
     return 0;
+}
+
+int convert_hour(int minute)
+{
+    if (minute < 60)
+    {
+        return 0;
+    } else 
+    {
+        return (int)(minute / 60);
+    }
+}
+
+int convert_minute(int minute)
+{
+    if (minute <= 1)
+    {
+        return 0;
+    }
+    else
+    {
+        return minute;
+    }
+}
+
+int convert_second(int minute)
+{
+    if (minute <= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return minute * 60;
+    }
 }
