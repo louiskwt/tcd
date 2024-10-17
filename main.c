a26e5f4cdc171fe92c55d3522a3e810e12fa553f
Louis Tsang <kawingt2@gmail.com>
2024-10-17 12:13:44 +0800

    displayed count down properly in ncurse window

--- main.c
+++ main.c
@@ -10,24 +10,33 @@ int main (void)
     clock_t begin = clock();
     int count = 60;
     double time_spent = 0;
+    double prev_time = 0;
 
+    // start ncurses window
     initscr();
-
-    addstr("---------------------------- Hello ------------------------");
     refresh();
-    addstr("\npress any key to exit...");
+    curs_set(0); // remove cursor
 
+    // count down logic
+    do
+    {
+        mvprintw(10, 10, "00 : 00 : %02i", count);
+        refresh();
+        time_spent = (double)(clock() - begin) / CLOCKS_PER_SEC;
+        if (time_spent - prev_time >= 1)
+        {
+            count--;
+            prev_time++;
+        }
+    }
+    while(time_spent <= 60);
+
+    // wait for key press and close window
+    mvprintw(10, 10, "\npress any key to exit...");
+    refresh();
     getch();
     endwin();
 
-    // // count down logic
-    // do
-    // {
-    //     printf("Counting down...  %f\n", time_spent);
-    //     time_spent = (double)(clock() - begin) / CLOCKS_PER_SEC;
-    // }
-    // while(time_spent <= 60);
 
-    // return when finished
     return 0;
 }
