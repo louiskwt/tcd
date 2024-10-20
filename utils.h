bcb595c3d3092beb105bfb48ffacbd31027bcd7f
Louis Tsang <kawingt2@gmail.com>
2024-10-20 18:10:51 +0800

    refactored util function

--- utils.h
+++ utils.h
@@ -5,23 +5,16 @@ int convert_minute(int second);
 int convert_second(int second);
 
 
-int convert_hour(int minute)
-{ 
-    if (minute < 60)
-    {
-        return 0;
-    } else 
-    {
-        return round(minute / 60);
-    }
+int convert_hour(int second)
+{
+    int min = round(second / 60);
+    return min < 60 ? 0 : round(min / 60);
 }
 
 int convert_minute(int second)
 {
-    if (second < 60)
-    {
-        return 0;
-    }
+    if (second < 60) return 0;
+    
     int min = round(second / 60);
     return min > 59 ? 0 : min;
 }
